99e80dea621cfbea4dff858d155e3d1e928c25c9
adnan-ahmed-khateeb <adnan.ahmed@attentive.ai>
2022-10-05 19:53:24 +0530

    console.log(userRole) for easily seeing which user is viewing the page right now

--- src/config/Can.ts
+++ src/config/Can.ts
@@ -7,10 +7,12 @@ const AbilityContext = createContext<AnyAbility>({} as AnyAbility);
 const Can = createContextualCan(AbilityContext.Consumer);
 
 const createAbility = (userRole: string) => {
-  console.log('userRole:', userRole)
+  console.log('userRole:', userRole); // To help see the user in console!
+
   const ability = createMongoAbility(
     permissions[userRole as keyof typeof permissions]
   );
+
   return ability;
 };
 
