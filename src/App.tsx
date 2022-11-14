0027b1aa2511dfd5237a20a90b09d7b115654000
adnan-ahmed-khateeb <adnan.ahmed@attentive.ai>
2022-11-14 18:46:12 +0530

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-import { useContext } from 'react';
-import './App.css';
-import Form from './components/Form';
-import { UserContext, UserProvider } from './context/UserContext';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Table from './components/Table';
-import { AbilityContext, createAbility } from './config/Can';
-import Home from './components/Home';
-import SelectUser from './components/SelectUser';
+import { useContext } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import { UserContext, UserProvider } from "./context/UserContext";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Table from "./components/Table";
+import { AbilityContext, createAbility } from "./config/Can";
+import Home from "./components/Home";
+import SelectUser from "./components/SelectUser";
 
 function App() {
-  const { userRole } = useContext(UserContext);
-  const ability = createAbility(userRole);
+   const { userRole } = useContext(UserContext);
+   const ability = createAbility(userRole);
 
-  return (
-    <UserProvider>
-      <Router>
-        <nav className='w-100 bg-gray-700 text-white font-bold h-12 flex justify-around items-center'>
-          <Link to='/' className='text-xl'>
-            Home
-          </Link>
-          <Link to='/table' className='text-xl'>
-            Table
-          </Link>
-          <Link to='/form' className='text-xl'>
-            Form
-          </Link>
-          <Link to='/selectuser' className='text-xl'>
-            Select User
-          </Link>
-        </nav>
-        {/* CASL's context provider */}
-        <AbilityContext.Provider value={ability}>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/form' element={<Form />} />
-            <Route path='/table' element={<Table />} />
-            <Route path='/selectuser' element={<SelectUser />} />
-          </Routes>
-        </AbilityContext.Provider>
-      </Router>
-    </UserProvider>
-  );
+   return (
+      <UserProvider>
+         <Router>
+            <nav className='w-100 bg-gray-700 text-white font-bold h-12 flex justify-around items-center'>
+               <Link to='/' className='text-xl'>
+                  Home
+               </Link>
+               <Link to='/table' className='text-xl'>
+                  Table
+               </Link>
+               <Link to='/form' className='text-xl'>
+                  Form
+               </Link>
+               <Link to='/selectuser' className='text-xl'>
+                  Select User
+               </Link>
+            </nav>
+            {/* CASL's context provider */}
+            <AbilityContext.Provider value={ability}>
+               <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/form' element={<Form />} />
+                  <Route path='/table' element={<Table />} />
+                  <Route path='/selectuser' element={<SelectUser />} />
+               </Routes>
+            </AbilityContext.Provider>
+         </Router>
+      </UserProvider>
+   );
 }
 
 export default App;
--- src/components/SelectUser.tsx
+++ src/components/SelectUser.tsx
@@ -2,7 +2,6 @@ import { useAbility } from "@casl/react";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AbilityContext } from "../config/Can";
-import { permissions } from "../config/PermissionsMap";
 import updateAbility from "../config/UpdateAbility";
 import { UserContext } from "../context/UserContext";
 
@@ -10,27 +9,23 @@ const SelectUser = () => {
    const { setUserRole } = useContext(UserContext);
 
    const ability = useAbility(AbilityContext);
-   const navigate = useNavigate()
+   const navigate = useNavigate();
 
    const handleChange = (role: string) => {
       setUserRole(role);
       updateAbility(ability, role);
-      navigate('/')
+      navigate("/");
    };
 
    return (
       <div className='flex flex-col w-fit p-4 m-auto my-auto justify-center h-[80vh] gap-3 text-gray-100 font-bold [&>button]:p-3 [&>button]:rounded-lg [&>button]:w-2/3 [&>button]:mx-auto'>
-         <p className="text-black">View src/config/PermissionsMap.ts to see user roles & permissions</p>
-         <button
-            onClick={() => handleChange("admin")}
-            className='bg-red-500'
-         >
+         <p className='text-black'>
+            View src/config/PermissionsMap.ts to see user roles & permissions
+         </p>
+         <button onClick={() => handleChange("admin")} className='bg-red-500'>
             Switch to Admin
          </button>
-         <button
-            onClick={() => handleChange("user")}
-            className='bg-blue-500'
-         >
+         <button onClick={() => handleChange("user")} className='bg-blue-500'>
             Switch to User
          </button>
          <button
