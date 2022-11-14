8490dc48c9099cc606a58fbcd3f9aa2dc035f0de
adnan-ahmed-khateeb <adnan.ahmed@attentive.ai>
2022-11-14 18:06:06 +0530

    Updated POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import './App.css';
 import Form from './components/Form';
 import { UserContext, UserProvider } from './context/UserContext';
--- src/components/SelectUser.tsx
+++ src/components/SelectUser.tsx
@@ -1,29 +1,52 @@
-import { useAbility } from '@casl/react';
-import { useContext } from 'react';
-import { AbilityContext } from '../config/Can';
-import updateAbility from '../config/UpdateAbility';
-import { UserContext } from '../context/UserContext';
+import { useAbility } from "@casl/react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AbilityContext } from "../config/Can";
+import { permissions } from "../config/PermissionsMap";
+import updateAbility from "../config/UpdateAbility";
+import { UserContext } from "../context/UserContext";
 
 const SelectUser = () => {
-  const { setUserRole } = useContext(UserContext);
+   const { setUserRole } = useContext(UserContext);
 
-  const ability = useAbility(AbilityContext);
+   const ability = useAbility(AbilityContext);
+   const navigate = useNavigate()
 
-  const handleChange = (role: string) => {
-    setUserRole(role);
-    updateAbility(ability, role);
-  };
+   const handleChange = (role: string) => {
+      setUserRole(role);
+      updateAbility(ability, role);
+      navigate('/')
+   };
 
-  return (
-    <div className='flex flex-col w-fit m-auto my-6'>
-      <button onClick={() => handleChange('admin')}>Switch to Admin</button>
-      <button onClick={() => handleChange('user')}>Switch to User</button>
-      <button onClick={() => handleChange('teamLead')}>
-        Switch to Team Lead
-      </button>
-      <button onClick={() => handleChange('manager')}>Switch to Manager</button>
-    </div>
-  );
+   return (
+      <div className='flex flex-col w-fit p-4 m-auto my-auto justify-center h-[80vh] gap-3 text-gray-100 font-bold [&>button]:p-3 [&>button]:rounded-lg [&>button]:w-2/3 [&>button]:mx-auto'>
+         <p className="text-black">View src/config/PermissionsMap.ts to see user roles & permissions</p>
+         <button
+            onClick={() => handleChange("admin")}
+            className='bg-red-500'
+         >
+            Switch to Admin
+         </button>
+         <button
+            onClick={() => handleChange("user")}
+            className='bg-blue-500'
+         >
+            Switch to User
+         </button>
+         <button
+            onClick={() => handleChange("teamLead")}
+            className='bg-slate-500'
+         >
+            Switch to Team Lead
+         </button>
+         <button
+            onClick={() => handleChange("manager")}
+            className='bg-green-500'
+         >
+            Switch to Manager
+         </button>
+      </div>
+   );
 };
 
 export default SelectUser;
